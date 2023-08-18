001205f2c3c404f59aba61d5c21f7223caa189f3
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2023-08-18 10:07:19 +0700

    add build-tools

---
+++ build-tools/README.md
@@ -0,0 +1,27 @@
+# KOOMPIOS ISO build scripts
+The KOOMPIOS build scripts are used to build the KOOMPIOS installation ISO images.
+
+## Building the ISO
+### 1. Install the build tools
+```
+sudo pacman -S archiso
+```
+### 2. Pull the repository
+```
+git clone https://github.com/KOOMPI-os/shifting.git
+```
+### 3. Build
+```
+cd shifting/build-tools
+sudo mkarchiso -v -w workdir/ -o out/ .
+```
+Once the building process is finished you can find the ISO image in the `./out` directory.
+### 4. Rebuilding the ISO
+When building a second time you will have to remove the `./out` and `./workdir` directories.
+```
+sudo rm -rf ./out ./workdir
+```
+
+## Credits
+This is based on Arkane Linux's ISO build scripts.
+
---
+++ build-tools/profiledef.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="KOOMPIOS-beta2"
+iso_label="KOOMPIOS_$(date +%Y%m)-beta2"
+iso_publisher="KOOMPIOS <https://koompi.org"
+iso_application="KOOMPIOS Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="koompi"
+buildmodes=('iso')
+bootmodes=(
+  'bios.syslinux.mbr'
+  'bios.syslinux.eltorito'
+  'uefi-ia32.grub.esp'
+  'uefi-ia32.grub.eltorito'
+	'uefi-x64.systemd-boot.esp'
+	'uefi-x64.systemd-boot.eltorito'
+)
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '15' '-b' '1M')
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/generate_locale"]="0:0:755"
+  ["/etc/default/useradd"]="0:0:600"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers"]="0:0:400"
+  ["/etc/locale.gen"]="0:0:644"
+)
