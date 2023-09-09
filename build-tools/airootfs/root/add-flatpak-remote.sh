2e744081e371fea34768da418f28c8741db41131
Hangsia Hong <44630728+hangsiahong@users.noreply.github.com>
2023-09-09 08:40:30 +0700

    update

--- build-tools/airootfs/root/add-flatpak-remote.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Add Flathub remote
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
--- build-tools/airootfs/root/customize_airootfs.sh
+++ build-tools/airootfs/root/customize_airootfs.sh
@@ -3,5 +3,3 @@
 set -e -u
 systemctl enable gdm
 systemctl set-default graphical.target
-
-./root/add-flatpak-remote.sh
