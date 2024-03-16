471429f90255a5c37938edfa9a202afaf428b221
Hangsia Hong <honghangsia@gmail.com>
2024-03-16 21:41:32 +0700

    add jade-gui koompi installation tools source code

---
+++ package-sources/jade-gui/jade_gui/manualpartitioning.py
@@ -0,0 +1,16 @@
+filesystems = [
+    "don't format",
+    "fat",
+    "xfs",
+    "btrfs",
+    "ext2",
+    "ext4",
+    "f2fs",
+]
+
+mountpoints = [
+    "none",
+    "System",
+    "Boot",
+    "User",
+]
