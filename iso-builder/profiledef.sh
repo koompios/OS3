eb7269aa46dab5b8fe3b76fb7ac785fcf3868fa8
hangsiahong <honghangsia@gmail>
2024-04-02 09:54:45 +0700

    add patch

--- iso-builder/profiledef.sh
+++ iso-builder/profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="KOOMPIOS-3.0.0"
-iso_label="KOOMPIOS_$(date +%Y%m)"
+iso_name="KOOMPIOS-3.0.0-mini"
+iso_label="KOOMPIOS_$(date +%Y%m)-mini"
 iso_publisher="KOOMPIOS <https://koompi.org"
 iso_application="KOOMPIOS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
