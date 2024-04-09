97a850c732fb929c10f341d8abf8d1b84cc24770
hangsia hong <honghangsia@gmail>
2024-04-09 23:23:58 +0700

    update

--- iso-builder/profiledef.sh
+++ iso-builder/profiledef.sh
@@ -24,6 +24,6 @@ file_permissions=(
   ["/etc/default/useradd"]="0:0:600"
   ["/etc/gshadow"]="0:0:400"
   ["/etc/sudoers"]="0:0:400"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/locale.gen"]="0:0:644"
 )
