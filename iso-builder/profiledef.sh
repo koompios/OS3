8b1ad9ff0f0b4927a50ee532343672e1912cb9db
hangsiahong <honghangsia@gmail.com>
2024-05-23 22:13:50 +0700

    update

--- iso-builder/profiledef.sh
+++ iso-builder/profiledef.sh
@@ -14,17 +14,16 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 #airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '10' '-b' '1M')
-
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/gnome-terminal"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/generate_locale"]="0:0:755"
   ["/etc/default/useradd"]="0:0:600"
   ["/etc/gshadow"]="0:0:400"
   ["/etc/sudoers"]="0:0:400"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/locale.gen"]="0:0:644"
 )
